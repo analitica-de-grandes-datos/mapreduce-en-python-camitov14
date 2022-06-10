31a5ea10c5a880c3d25570c6a41b1f4b5b2a9593
camitov14 <102377531+camitov14@users.noreply.github.com>
2022-06-10 11:30:45 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -3,10 +3,9 @@
 #
 import sys
 if __name__ == "__main__":
-    for line in sys.stdin.readlines():
-        for column in line:
-            column=[x.split(",") for x in column]
-            column=[x[3] for x in column]
-            for word in column:
-                sys.stdout.write("{}\t1\n".format(word))
+    data=sys.stdin.readlines()
+    line=[x.split(",") for x in data]
+    column=[x[3] for x in line]
+    for word in column:
+            sys.stdout.write("{}\t1\n".format(word))
 
