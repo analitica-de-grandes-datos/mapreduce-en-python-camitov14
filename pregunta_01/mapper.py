3d47b08ade6ac97b5a9cc1969f1ddaa6a3ec90f2
camitov14 <102377531+camitov14@users.noreply.github.com>
2022-06-10 10:59:07 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
     for line in sys.stdin:
         line = [x.split("\t") for x in line]
-        for column in line.split(","):
+        for column in line:
+            column=[x.split(",") for x in column]
             for word in column[3]:
                 sys.stdout.write("{}\t1\n".format(word))
